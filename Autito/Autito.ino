d34ce7d35a5bc6e178011745c73b9e21c1b2eef3
GuiralRocio <144493026+GuiralRocio@users.noreply.github.com>
2023-11-29 11:50:00 -0300

    Update Autito.ino

--- Autito/Autito.ino
+++ Autito/Autito.ino
@@ -6,6 +6,11 @@
 int bluetoothTx = 2;
 int bluetoothRx = 3;
 
+//CONEXIONES SENSOR
+
+const int Trigger = 5;   //Pin digital 5 para el Trigger del sensor
+const int Echo = 6;   //Pin digital 6 para el echo del sensor
+
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 //MOTOR 1
@@ -26,6 +31,10 @@ void setup() {
   bluetooth.println("U.9600.N");
   bluetooth.begin(9600);
 
+  pinMode(Trigger, OUTPUT);
+  pinMode(Echo, INPUT); 
+  digitalWrite(Trigger, LOW);
+  
   pinMode( Motor1A, OUTPUT );
   pinMode( Motor2A, OUTPUT );
   pinMode( Motor1B, OUTPUT );
@@ -43,6 +52,21 @@ int flag2 = -1;
 
 void loop() {
 
+  if (bluetooth.available()){
+    long t; //timepo que demora en llegar el eco
+    long d; //distancia en centimetros
+
+    digitalWrite(Trigger, HIGH);
+    delayMicroseconds(10);          //Enviamos un pulso de 10us
+    digitalWrite(Trigger, LOW);
+  
+    t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+    d = t/59;             //escalamos el tiempo a una distancia en cm
+    // el 59 sale de :v=d/t y se que vel del sonido 340m/s,el tiempo fue medido en us y para obtener la dist en centimetros hay ue hacer regl de 3
+    bluetooth.print(u);
+    delay(100);
+  }
+  
   if (bluetooth.available()){
     char toSend = (char)bluetooth.read();
     if(toSend == 'S'){
