b0f7a705438189490a62d42fb417cd90eae81716
GuiralRocio <144493026+GuiralRocio@users.noreply.github.com>
2023-12-13 15:41:50 -0300

    Update Autito.ino

--- Autito/Autito.ino
+++ Autito/Autito.ino
@@ -1,17 +1,11 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
-
 //CONEXIONES PARA EL BLUETOOTH
 
 int bluetoothTx = 2;
 int bluetoothRx = 3;
 
-//CONEXIONES SENSOR
-
-const int Trigger = 5;   //Pin digital 5 para el Trigger del sensor
-const int Echo = 6;   //Pin digital 6 para el echo del sensor
-
-SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+//SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 //MOTOR 1
 
@@ -23,13 +17,38 @@ int Motor1B = 10;
 int Motor2A = 11;
 int Motor2B = 12;
 
+//CONEXIONES DEL SENSOR
+
+const int Trigger = 5;   //Pin digital 2 para el Trigger del sensor
+const int Echo = 6;   //Pin digital 3 para el echo del sensor
+
+int u;
+
+class Auto{
+  
+  private:
+    char toSend;
+    int flag1=-1;
+    int flag2=-1;
+  
+  public:
+    char asignatoSend();
+    void detenerse();
+    void avanza();
+    void retrocede();
+    void derecha();
+    void izquierda();
+    void otro();
+};
 void setup() {
 
-  bluetooth.begin(115200);
-  bluetooth.print("$$$");
-  delay(100);
-  bluetooth.println("U.9600.N");
-  bluetooth.begin(9600);
+  //para inicializar el modulo
+  //bluetooth.begin(115200);
+  Serial.begin(9600);//para comunicarse con precessing
+  //bluetooth.print("$$$");
+  delay(10);
+  //bluetooth.println("U.9600.N");
+  
 
   pinMode(Trigger, OUTPUT);
   pinMode(Echo, INPUT); 
@@ -44,40 +63,72 @@ void setup() {
   digitalWrite( Motor2A, LOW );
   digitalWrite( Motor1B, LOW );
   digitalWrite( Motor2B, LOW );
-
 }
 
-int flag1 = -1;
-int flag2 = -1;
+Auto MyCar;
 
 void loop() {
 
-  if (bluetooth.available()){
-    long t; //timepo que demora en llegar el eco
-    long d; //distancia en centimetros
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
 
-    digitalWrite(Trigger, HIGH);
-    delayMicroseconds(10);          //Enviamos un pulso de 10us
-    digitalWrite(Trigger, LOW);
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
   
-    t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
-    d = t/59;             //escalamos el tiempo a una distancia en cm
-    // el 59 sale de :v=d/t y se que vel del sonido 340m/s,el tiempo fue medido en us y para obtener la dist en centimetros hay ue hacer regl de 3
-    bluetooth.print(u);
-    delay(100);
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d = t/59; //escalamos el tiempo a una distancia en cm
+
+ /* if (d<10){
+    u = d;
+  }else{
+    u=0;
+  }  */          
+  
+ // Serial.print("Distancia: ");
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+ // Serial.print("cm");
+  Serial.println();
+  delay(100);
+
+  if (Serial.available()){
+    char toSend=MyCar.asignatoSend();
+     if(toSend == 'S'){
+      MyCar.detenerse();
+     }
+     if(toSend == 'F' || toSend == 'G' || toSend == 'I'){
+      MyCar.avanza();
+      }
+      if(toSend == 'B' || toSend == 'H' || toSend == 'J'){
+        MyCar.retrocede();
+        }
+      if(toSend == 'L' || toSend == 'G' || toSend == 'I'){
+       MyCar.izquierda();
+        }
+      else if(toSend == 'R' || toSend == 'I' || toSend == 'J'){
+      MyCar.derecha();
+      }
+    }else{
+      MyCar.otro();
+    }
+    
   }
-  
-  if (bluetooth.available()){
-    char toSend = (char)bluetooth.read();
-    if(toSend == 'S'){
-      flag1 = 0;
-      flag2 = 0;
+
+  char Auto::asignatoSend(){
+    toSend = (char)Serial.read();
+    return toSend;
+  }
+  void Auto::detenerse(){
+     flag1 = 0;
+     flag2 = 0;
       digitalWrite(Motor1A, LOW);
       analogWrite(Motor1B, LOW);
       digitalWrite(Motor2A, LOW);
-      analogWrite(Motor2B, LOW);
-      }
-    if(toSend == 'F' || toSend == 'G' || toSend == 'I'){
+      analogWrite(Motor2B, LOW);   
+  }
+  
+  void Auto::avanza(){
+    
       if(flag1 != 1){ //hace que se mueva hacia adelante
         flag1 = 1;
         digitalWrite(Motor1A, HIGH);
@@ -87,43 +138,47 @@ void loop() {
       }
     }
     
-    if(toSend == 'B' || toSend == 'H' || toSend == 'J'){
-      if(flag1 != 2){ //hace que se mueva hacia atras
+  void Auto::retrocede(){     
+    
+    if(flag1 != 2){ //hace que se mueva hacia atras
         flag1 = 2;
         digitalWrite(Motor1B, HIGH);
         analogWrite(Motor1A, 0);
         digitalWrite(Motor2B, HIGH);
         analogWrite(Motor2A, 0);
-      }
-    }
-
-    if(toSend == 'L' || toSend == 'G' || toSend == 'I'){
-      if(flag2 != 1){ //hace que gire hacia la izquierda
-        flag2 = 1;
-        digitalWrite(Motor2B, HIGH);
-        analogWrite(Motor2A, 0);
-        digitalWrite(Motor1A, HIGH);
-        analogWrite(Motor1B, 0);
-      }
-    }
-
-    else if(toSend == 'R' || toSend == 'I' || toSend == 'J'){
-      if(flag2 != 2){ //hace que gire hacia la derecha
+     }
+    
+  }
+  
+  void Auto::izquierda(){
+  
+    if(flag2 != 2){ //hace que gire hacia la izquierda
         flag2 = 2;
         digitalWrite(Motor1B, HIGH);
         analogWrite(Motor1A, 0);
         digitalWrite(Motor2A, HIGH);
         analogWrite(Motor2B, 0);
-      }
+      
+       }
+     }
+  void Auto::derecha(){
+    
+    if(flag2 != 1){ //hace que gire hacia la derecha
+        flag2 = 1;
+        digitalWrite(Motor2B, HIGH);
+        analogWrite(Motor2A, 0);
+        digitalWrite(Motor1A, HIGH);
+        analogWrite(Motor1B, 0);
     }
-    else{
-      if(flag2 != 3){
+  }
+        
+  void Auto::otro(){
+      
+    if(flag2 != 3){
         flag2 = 3;
         digitalWrite(Motor2A, LOW);
         analogWrite(Motor2B, LOW);
         digitalWrite(Motor2A, LOW);
         analogWrite(Motor2B, LOW);
-      }
     }
   }
-}
